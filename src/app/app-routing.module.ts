d34fb68db0db44a62da4f2455c563e0fc6bcdf3f
Rafal Brzoska <rbrzoska@future-processing.com>
2021-05-31 08:37:31 +0200

    menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
+import { SubjectsComponent } from './subjects/subjects.component';
+import { ObservablesComponent } from './observables/observables.component';
+import { TestComponent } from './test/test.component';
+import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'test', component: TestComponent},
+  {path: 'observables', component: ObservablesComponent},
+  {path: 'subjects', component: SubjectsComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ObservablesComponent } from './observables/observables.component';
+import { SubjectsComponent } from './subjects/subjects.component';
+import { TestComponent } from './test/test.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ObservablesComponent,
+    SubjectsComponent,
+    TestComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
