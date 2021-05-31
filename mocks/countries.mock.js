a7a798f71718889b15ea7fbda0bb11f1c2061d8d
Rafal Brzoska <rbrzoska@future-processing.com>
2021-05-31 08:05:54 +0200

    API

---
+++ mocks/countries.mock.js
@@ -0,0 +1,9 @@
+module.exports = {
+  path: '/countries',
+  method: 'GET',
+  template: [
+    { id: 'pl', name: 'Polska'},
+    { id: 'de', name: 'Niemcy'},
+    { id: 'gb', name: 'Wielka Brytania'}
+  ],
+};
---
+++ mocks/users.mock.js
@@ -0,0 +1,9 @@
+module.exports = {
+  path: '/users',
+  method: 'GET',
+  template: [
+    { id: '1', name: 'Rafal'},
+    { id: '2', name: 'Alojzy'},
+    { id: '3', name: 'Jarosław'}
+  ],
+};
